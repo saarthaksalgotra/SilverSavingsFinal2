9c6d2ead48ed8ed4159ee84e41d65d7a10b2522b
Saarthak <133770011+saarthaksalgotra@users.noreply.github.com>
2024-10-18 00:38:33 +0530

    update app.js

--- app.js
+++ app.js
@@ -18,11 +18,68 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.get('/', (req, res) => res.send("HELLO"));
+app.get('/', (req, res) => res.send("HELLO")); 
 
-// Your API routes here (e.g., /register, /login, etc.)
+app.post('/register', async (req, res) => {
+    const { email, password } = req.body; 
+    const user = new User({ email, password }); 
+    try {
+        await user.save(); 
+        res.status(201).json({msg: "Registration successful"})
+    } catch(err) {
+        res.status(400).json({error: err.message})
+    }
 
-// Deployment Configuration
+})
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body; 
+    const account = await User.findOne().where('email').equals(email);
+    if (!account) return res.status(404).json({success: false, msg: "Account does not exist"})
+
+    const match = account.password == password; 
+    if (!match) return res.status(400).json({success: false, msg: "Invalid password"}); 
+
+    res.status(200).json({success: true, userID: account._id})
+})
+
+app.post('/expenses', async (req, res) => {
+    const { amount, text, userID } = req.body;
+    const user = await User.findById(userID); 
+    if (!user) return res.status(404).json({msg: "Not logged in"});
+    
+    try {
+        const expense = await Expense.create({ amount, text }); 
+        user.expenses.push(expense); 
+        await user.save();
+
+        res.send({ msg: "Expense created successfully", expense}); 
+
+    } catch(err) {
+        res.status(400).send({ error: err.message })
+    }
+
+})
+
+app.delete('/expenses/:id', async (req, res) => {
+    const id = req.params.id; 
+    try {
+        await Expense.findByIdAndDelete(id); 
+        res.send({msg: "Deleted successfully"}); 
+    } catch(err) {
+        res.status(400).json({error: err.message}); 
+    }
+})
+
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id; 
+    const user = await User.findById(id).populate('expenses');; 
+    if (!user) return res.status(404).json({err: "User not found"}); 
+
+    res.send({ user }); 
+
+})
+
 const __dirname1 = path.resolve(); // Use a single __dirname variable
 
 console.log(process.env.NODE_ENV === "production"); 
